e2e57cb8644efaaa76cb79ceea2f6f6c4ee54393
mugiwara2070 <101516333+mugiwara2070@users.noreply.github.com>
2022-07-20 15:31:41 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         print("hello world from main branch");
+        print("hello world from github");
     }
 
     private static void print(String s){
