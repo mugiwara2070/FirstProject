4496f472c0b2a93ad025e8da68690270462b34ef
khalil rahmouni <khalil.rahmouni13@gmail.com>
2022-07-20 15:11:45 +0200

    add  print_1

--- Main.java
+++ Main.java
@@ -7,6 +7,9 @@ public class Main {
     private static void print(String s){
         System.out.println(s);
     }
+    private static void print_1(String s){
+        System.out.println(s);
+    }
 }
 
 
